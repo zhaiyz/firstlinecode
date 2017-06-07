92f12c5ae60b4e7d78a6baac467938f39d4f2974
zhaiyz <zhaiyz@aliyun.com>
2017-06-07 22:20:37 +0800

    add some test projects

--- DatabaseTest/app/src/main/java/com/zhaiyz/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/zhaiyz/databasetest/MyDatabaseHelper.java
@@ -18,6 +18,11 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             "pages integer, " +
             "name text)";
 
+    public static final String CREATE_CATEGORY = "create table Category (" +
+            "id integer primary key autoincrement, " +
+            "category name text, " +
+            "category_code integer)";
+
     private Context mContext;
 
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
@@ -29,11 +34,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Created succeeded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }
---
+++ LitePalTest/app/src/main/java/com/zhaiyz/litepaltest/Category.java
@@ -0,0 +1,38 @@
+package com.zhaiyz.litepaltest;
+
+/**
+ * Created by zhaiyz on 17-6-7.
+ */
+
+public class Category {
+
+    private int id;
+
+    private String categoryName;
+
+    private int categoryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
